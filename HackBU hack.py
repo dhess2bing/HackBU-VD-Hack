7549c1e35721c61d37905fd50a98df7862e5f4c3
Dillon Hess <dhess2@binghamton.edu>
2019-02-09 15:42:22 -0500

    Update HackBU hack.py

--- HackBU hack.py
+++ HackBU hack.py
@@ -6,12 +6,12 @@ frame.pack()
 bottomframe = tkinter.Frame(root)
 
 
-redbutton = tkinter.Button(frame, text="Red", fg="red")
+redbutton = tkinter.Button(frame, text="Red", fg="red", bd = 5, font ="Times")
 redbutton.grid()
-greenbutton = tkinter.Button(frame, text="Brown", fg="brown")
+greenbutton = tkinter.Button(frame, text="Brown", fg="brown", font = "Times")
 greenbutton.grid()
 
-bluebutton = tkinter.Button(frame, text="Blue", fg="blue")
+bluebutton = tkinter.Button(frame, text="Blue", fg="blue", font = "Times", height = 2)
 bluebutton.grid()
 blackbutton = tkinter.Button(bottomframe, text="Black", fg="black")
 blackbutton.grid()
