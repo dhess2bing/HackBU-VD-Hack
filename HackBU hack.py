83932b1bb71013e322700b2c1fa20139ac30e081
Dillon Hess <dhess2@binghamton.edu>
2019-02-09 17:05:43 -0500

    Update HackBU hack.py

--- HackBU hack.py
+++ HackBU hack.py
@@ -5,9 +5,23 @@ def main():
     root = tkinter.Tk()
     frame = tkinter.Frame(root)
     frame.pack()
+    root.geometry("500x400+400+150")
     bottomframe = tkinter.Frame(root)
-    bluebutton = tkinter.Button(frame, text="Blue", fg="blue", font = "Times", height = 2)
-    bluebutton.grid()
-    blackbutton = tkinter.Button(bottomframe, text="Black", fg="black")
-    blackbutton.grid()
+
+    vd_str = "Valentine's Day Card Generator"
+    title_label = tkinter.Label(frame, text = vd_str )
+    title_label.pack(side = tkinter.TOP)
+    
+    redbutton = tkinter.Button(frame, text="Red", fg="red", bd = 5, font ="Times")
+    redbutton.pack(side = tkinter.TOP)
+
+    
+    greenbutton = tkinter.Button(frame, text="Brown", fg="brown", font = "Times")
+    greenbutton.pack(side = tkinter.TOP)
+
+
+    
+
     root.mainloop()
+
+main()
