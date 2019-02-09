af4676696a899979adb257fb60109416aab9b5af
Dillon Hess <dhess2@binghamton.edu>
2019-02-09 18:46:42 -0500

    Update HackBU hack.py

--- HackBU hack.py
+++ HackBU hack.py
@@ -1,32 +1,82 @@
 import tkinter
-import tkinter.messagebox
 
-def main():
-    root = tkinter.Tk()
-    frame = tkinter.Frame(root)
-    grid_frame = tkinter.Frame(root)
-    grid_frame.grid()
-    frame.grid()
-    
-    root.geometry("500x400+400+150")
-    bottomframe = tkinter.Frame(root)
+def Start():#goes from menu to card
+    name1 = reciever.get()
+    name2 = gifter.get()
+    welcome1.destroy()
+    reciever.destroy()
+    welcome2.destroy()
+    gifter.destroy()
+    go.destroy()
+    create1()
+def create1():
+    pic = tkinter.Label(top,text="Insert Image here")
+    pic.pack()
+    pic.place(x = 275,y = 225)
+    words = tkinter.Label(top,text="Insert quote here")
+    words.pack()
+    words.place(x = 275, y = 625)
+    another.grid()
+    menu = tkinter.Button(master = top,text="Return",command = Return)
+    menu.pack()
+def Repeat(pic,words):
+    pic.destroy()
+    words.destroy()
+    create1()
+def Return():#goes back to menu
+    pic.destroy()
+    words.destroy()
+    another.destroy()#works
+    menu.destroy()
+    welcome1 = tkinter.Label(top,text="Insert the Recipient's name here")
+    welcome1.pack()
+    welcome1.place(x = 275, y = 250)
+    reciever = tkinter.Entry(top)
+    reciever.pack()
+    reciever.place(x = 275, y = 275)
+    welcome2 = tkinter.Label(top,text="Insert the Sender's name here")
+    welcome2.pack()
+    welcome2.place(x = 275, y = 300)
+    gifter = tkinter.Entry(top)
+    gifter.pack()
+    gifter.place(x = 275, y = 325)
+    go = tkinter.Button(top,text="Generate", command=Start)
+    go.pack()
+    go.place(x = 275, y = 350)
+top = tkinter.Tk()
+top.geometry("650x750");
+top.resizable(0,0);
+back = tkinter.Frame(master=top,bg="white");
+back.pack_propagate(0)
+back.pack(fill=tkinter.BOTH, expand=1)
+name1 = "";
+name2 = "";
+welcome1 = tkinter.Label(top,text="Insert the Recipient's name here")
+welcome1.pack()
+welcome1.place(x = 275, y = 250)
+reciever = tkinter.Entry(top)
+reciever.pack()
+reciever.place(x = 275, y = 275)
+welcome2 = tkinter.Label(top,text="Insert the Sender's name here")
+welcome2.pack()
+welcome2.place(x = 275, y = 300)
+gifter = tkinter.Entry(top)
+gifter.pack()
+gifter.place(x = 275, y = 325)
+go = tkinter.Button(top,text="Generate", command=Start)
+go.pack()
+go.place(x = 275, y = 350)
+pic = tkinter.Label(top,text="Insert Image here")
+words = tkinter.Label(top,text="Insert quote here")
+another = tkinter.Button(master = back, text="Repeat", command=Repeat)
+another.grid(row=1,column=0)
+menu = tkinter.Button(master = back,text="Return",command = Return)
+menu.grid(row=2,column=0)
+pic.destroy()
+words.destroy()
+another.grid_remove()
+menu.grid_remove()
 
-    vd_str = "Valentine's Day Card Generator"
-    title_label = tkinter.Label(frame, text = vd_str )
-    title_label.grid(row = 0, column = 3)
 
-    from_str = ("Who is sending this card? ")
-    from_label = tkinter.Label(frame, text = from_str)
-    from_label.grid(row = 2, column =2)
+top.mainloop()
 
-    entry_from_str = tkinter.Entry(frame, bd =4)
-    entry_from_str.grid(row = 2, column = 3)
-    
-    
-    redbutton = tkinter.Button(frame, text="Generate Card", fg="red", bd = 5, font ="Times")
-    redbutton.grid(row = 25, column = 3, rowspan = 15)
-    
-
-    root.mainloop()
-
-main()
