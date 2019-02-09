d4e8207f023c054ec2060417616a5f2fa320587d
Dillon Hess <dhess2@binghamton.edu>
2019-02-09 18:03:40 -0500

    Update HackBU hack.py

--- HackBU hack.py
+++ HackBU hack.py
@@ -4,22 +4,27 @@ import tkinter.messagebox
 def main():
     root = tkinter.Tk()
     frame = tkinter.Frame(root)
-    frame.pack()
+    grid_frame = tkinter.Frame(root)
+    grid_frame.grid()
+    frame.grid()
+    
     root.geometry("500x400+400+150")
     bottomframe = tkinter.Frame(root)
 
     vd_str = "Valentine's Day Card Generator"
     title_label = tkinter.Label(frame, text = vd_str )
-    title_label.pack(side = tkinter.TOP)
+    title_label.grid(row = 0, column = 3)
+
+    from_str = ("Who is sending this card? ")
+    from_label = tkinter.Label(frame, text = from_str)
+    from_label.grid(row = 2, column =2)
+
+    entry_from_str = tkinter.Entry(frame, bd =4)
+    entry_from_str.grid(row = 2, column = 3)
     
-    redbutton = tkinter.Button(frame, text="Red", fg="red", bd = 5, font ="Times")
-    redbutton.pack(side = tkinter.TOP)
-
     
-    greenbutton = tkinter.Button(frame, text="Brown", fg="brown", font = "Times")
-    greenbutton.pack(side = tkinter.TOP)
-
-
+    redbutton = tkinter.Button(frame, text="Generate Card", fg="red", bd = 5, font ="Times")
+    redbutton.grid(row = 25, column = 3, rowspan = 15)
     
 
     root.mainloop()
